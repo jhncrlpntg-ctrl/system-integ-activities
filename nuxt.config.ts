62c187acc38b8a8fe7018170683a256ed5a16e96
jhncrlpntg-ctrl <jcpantig15@gmail.com>
2026-06-22 14:59:46 +0800

    Add QR scanner and QR view pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
@@ -14,10 +15,14 @@ export default defineNuxtConfig({
         transformAssetUrls,
       },
     },
+    
+    optimizeDeps: {
+      include: ['qr-scanner']
+    },
+    assetsInclude: ['**/*.worker.js']
   },
-
   runtimeConfig: {
-    public:{
+    public: {
       //@ts-ignore
       googleClientId: process.env.NUXT_PUBLIC_GOOGLE_CLIENT_ID
     }
